56c10d097c0aacf1afa4b6bb0ecca5b6e8497267
ledoAc <164891135+ledoAc@users.noreply.github.com>
2024-06-27 11:26:21 +0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -213,8 +213,6 @@ fi
 
 
 
-
-
 if [ "$#" -eq 2 ]; then
     domain="$1"
     param="$2"
@@ -225,60 +223,62 @@ if [ "$#" -eq 2 ]; then
         web_serv=$(dig +short -x "$serv_a_records")
 
         if [[ "$web_serv" == *"web-hosting.com"* ]]; then
- server_record=$(dig +short -x "$serv_a_records" | cut -d'-' -f1)
-cuser=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com"  "sudo /scripts/whoowns $domain")
-echo "Select scan type:"
-echo "1. Scan"
-echo "2. Scan with quarantine"
-read -p "Your choice (1 or 2): " CHOICE
+            server_record=$(dig +short -x "$serv_a_records" | cut -d'-' -f1)
+            cuser=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com"  "sudo /scripts/whoowns $domain")
+            echo "Select scan type:"
+            echo "1. Scan"
+            echo "2. Scan with quarantine"
+            read -p "Your choice (1 or 2): " CHOICE
 
-case $CHOICE in
-  1)
-    scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report "/home/$cuser/scanreport-$cuser-$(date +%b_%d_%Y_%Hh%Mm).txt""")
-    ;;
-  2)
-    scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report "/home/$cuser/scanreport-$cuser-quarantine-$(date +%b_%d_%Y_%Hh%Mm).txt" --quarantine /opt/cxs/quarantine"")
-    ;;
-  *)
-    echo "Invalid choice. Please choose 1 or 2."
-    exit 1
-    ;;
-esac
+            case $CHOICE in
+              1)
+                scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report \"/home/$cuser/scanreport-$cuser-$(date +%b_%d_%Y_%Hh%Mm).txt\"")
+                ;;
+              2)
+                scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report \"/home/$cuser/scanreport-$cuser-quarantine-$(date +%b_%d_%Y_%Hh%Mm).txt\" --quarantine /opt/cxs/quarantine")
+                ;;
+              *)
+                echo "Invalid choice. Please choose 1 or 2."
+                exit 1
+                ;;
+            esac
 
-echo
-echo -e "\e[96m####################################################################################################################################################\e[0"
+            echo
+            echo -e "\e[96m####################################################################################################################################################\e[0"
 
         else
             while [[ -z "$server_record_new" ]]; do
                 read -p "Enter the full name of the server: " server_record_new
             done
-server_record=$(dig +short -x "$serv_a_records" | cut -d'-' -f1)
-cuser=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$serv_a_records"  "sudo /scripts/whoowns $domain")
-echo "Select scan type:"
-echo "1. Scan"
-echo "2. Scan with quarantine"
-read -p "Your choice (1 or 2): " CHOICE
+            server_record=$(dig +short -x "$serv_a_records" | cut -d'-' -f1)
+            cuser=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$serv_a_records"  "sudo /scripts/whoowns $domain")
+            echo "Select scan type:"
+            echo "1. Scan"
+            echo "2. Scan with quarantine"
+            read -p "Your choice (1 or 2): " CHOICE
 
-case $CHOICE in
-  1)
-    scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$serv_a_records" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report "/home/$cuser/scanreport-$cuser-$(date +%b_%d_%Y_%Hh%Mm).txt""")
-    ;;
-  2)
-    scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$serv_a_records" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report "/home/$cuser/scanreport-$cuser-quarantine-$(date +%b_%d_%Y_%Hh%Mm).txt" --quarantine /opt/cxs/quarantine"")
-    ;;
-  *)
-    echo "Invalid choice. Please choose 1 or 2."
-    exit 1
-    ;;
-esac
-echo -e "\e[96m####################################################################################################################################################\e[0"
-
+            case $CHOICE in
+              1)
+                scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$serv_a_records" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report \"/home/$cuser/scanreport-$cuser-$(date +%b_%d_%Y_%Hh%Mm).txt\"")
+                ;;
+              2)
+                scan=$(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$serv_a_records" "sudo /usr/local/sbin/cxs.sh --filemax 50000 -B --user $cuser --report \"/home/$cuser/scanreport-$cuser-quarantine-$(date +%b_%d_%Y_%Hh%Mm).txt\" --quarantine /opt/cxs/quarantine")
+                ;;
+              *)
+                echo "Invalid choice. Please choose 1 or 2."
+                exit 1
+                ;;
+            esac
+            echo -e "\e[96m####################################################################################################################################################\e[0"
         fi
+    else
+        echo "Invalid parameter. Only 'scan' is accepted."
+        exit 1
     fi
-
+else
+    echo "Usage: $0 domain scan"
+    exit 1
 fi
-
-
 
 
 
