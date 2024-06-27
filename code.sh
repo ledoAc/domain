17978270e549464cd88f08d74abbb41e3df18044
ledoAc <164891135+ledoAc@users.noreply.github.com>
2024-06-27 13:12:55 +0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -157,7 +157,7 @@ fi
 if [ "$#" -eq 2 ]; then
     param="$2"
 
-    if [ "$param" = "-w" ]; then
+    if [ "$param" = "-www" ]; then
         serv_a_records=$(dig +short +trace +nodnssec "$domain" A | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' | tail -n 2 | head -n 1)
         web_serv=$(dig +short -x "$serv_a_records")
 
