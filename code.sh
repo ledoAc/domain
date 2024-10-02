ae0e4c26cf801189ee6b7bb5295173954bb495a8
ledoAc <164891135+ledoAc@users.noreply.github.com>
2024-10-02 04:58:20 +0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -109,7 +109,21 @@ print_in_frame_dom "=====================| DOMLOGS |====================="
         fi
     fi
 done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -d" 2>/dev/null | tr -d '\0')
+echo
 
+found_mysql=false
+
+while IFS= read -r line; do
+    if $found_mysql; then
+        echo "$line"
+    else
+        if [[ $line == *"=====================| MYSQL |====================="* ]]; then
+            echo
+print_in_frame_dom "=====================| MYSQL |====================="
+            found_mysql=true
+        fi
+    fi
+done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -m" 2>/dev/null | tr -d '\0')
 
 
 echo
