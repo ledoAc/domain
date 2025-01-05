57f0fe67fd60bfecf7dfbeaf9c78447472d703d6
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-01-06 00:08:49 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -128,22 +128,22 @@ done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q 
 
 echo
 
-found_cpu_mem=false
+found_mysql=false
 
 while IFS= read -r line; do
-    # Перевірка, чи знайдено рядок "CPU & MEM"
-    if $found_cpu_mem; then
-        echo "$line"  # Просто вивести рядок
+    if $found_mysql; then
+        echo "$line"
     else
-        # Якщо рядок містить "=====================| CPU & MEM |====================="
-        if [[ "$line" == *"=====================| CPU & MEM |====================="* ]]; then
-            echo  # Вивести порожній рядок перед виведенням блоку
-            echo "=====================| CPU & MEM |====================="  # Вивести результат без кольору
-            found_cpu_mem=true
+        if [[ $line == *"=====================| MYSQL |====================="* ]]; then
+            echo
+print_in_frame_dom "=====================| MYSQL |====================="
+            found_mysql=true
         fi
     fi
-done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -p" 2>/dev/null | tr -d '\0')
+done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -m" 2>/dev/null | tr -d '\0')
 
+
+echo
 
 
 
