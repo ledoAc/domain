8f376c35527fef7de588a32b61e76eab36849441
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-01-06 00:28:56 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -111,6 +111,23 @@ print_in_frame_dom "=====================| DOMLOGS |====================="
 done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -d" 2>/dev/null | tr -d '\0')
 echo
 
+found_cpu_mem=false
+
+while IFS= read -r line; do
+    if $found_cpu_mem; then
+        # Зміна кольору поточного рядка, якщо вже знайдено заголовок
+        echo "$(tput setaf 2)$line$(tput sgr0)"  # зелений колір для рядка
+    else
+        if [[ $line == *"=====================| CPU & MEM |====================="* ]]; then
+            echo
+            # Виведення заголовка в кольорі
+            print_in_frame_dom "$(tput setaf 2)=====================| CPU & MEM |=====================$(tput sgr0)"
+            found_cpu_mem=true
+        fi
+    fi
+done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -p" 2>/dev/null)
+
+
 found_mysql=false
 
 while IFS= read -r line; do
@@ -128,21 +145,6 @@ done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q 
 
 echo
 
-found_cpu_mem=false
-
-while IFS= read -r line; do
-    if $found_cpu_mem; then
-        # Зміна кольору поточного рядка, якщо вже знайдено заголовок
-        echo "$(tput setaf 2)$line$(tput sgr0)"  # зелений колір для рядка
-    else
-        if [[ $line == *"=====================| CPU & MEM |====================="* ]]; then
-            echo
-            # Виведення заголовка в кольорі
-            print_in_frame_dom "$(tput setaf 2)=====================| CPU & MEM |=====================$(tput sgr0)"
-            found_cpu_mem=true
-        fi
-    fi
-done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -p" 2>/dev/null)
 
 
 
