18a908e690f0ba8e7df18aac951e0bd60436783a
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-01-06 00:25:31 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -81,7 +81,7 @@ if [ "$#" -eq 2 ]; then
 
 
 
-    if [ "$param" = "-c" ]; then
+   if [ "$param" = "-c" ]; then
 
 
 
@@ -111,8 +111,6 @@ print_in_frame_dom "=====================| DOMLOGS |====================="
 done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -d" 2>/dev/null | tr -d '\0')
 echo
 
-
-
 found_mysql=false
 
 while IFS= read -r line; do
@@ -128,6 +126,22 @@ print_in_frame_dom "=====================| MYSQL |====================="
 done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -m" 2>/dev/null | tr -d '\0')
 
 
+echo
+echo "Hello"
+
+found_cpu_mem=false
+
+while IFS= read -r line; do
+    if $found_cpu_mem; then
+        echo "$line"
+    else
+        if [[ $line == *"=====================| CPU & MEM |====================="* ]]; then
+            echo
+            print_in_frame_dom "=====================| CPU & MEM |====================="
+            found_cpu_mem=true
+        fi
+    fi
+done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -m" 2>/dev/null | tr -d '\0')
 echo
 
 
