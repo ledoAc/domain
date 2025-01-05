e4fbea2d206c337d9ce880b5ac565d7504398f94
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-01-06 00:19:15 +0200

    Update code.sh

--- code.sh
+++ code.sh
@@ -114,14 +114,17 @@ echo
 found_cpu_mem=false
 
 while IFS= read -r line; do
-    if [[ "$line" == *"CPU & MEM"* ]]; then
-        if ! $found_cpu_mem; then
-            # Зміна кольору за допомогою ANSI-кодів (наприклад, зелений)
+    if $found_cpu_mem; then
+        # Зміна кольору поточного рядка, якщо вже знайдено заголовок
+        echo "$(tput setaf 2)$line$(tput sgr0)"  # зелений колір для рядка
+    else
+        if [[ $line == *"=====================| CPU & MEM |====================="* ]]; then
+            echo
+            # Виведення заголовка в кольорі
             print_in_frame_dom "$(tput setaf 2)=====================| CPU & MEM |=====================$(tput sgr0)"
             found_cpu_mem=true
         fi
     fi
-    echo "$line"
 done < <(ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile=/dev/null" -q -p 12789 "wh@$server_record.web-hosting.com" "sudo /root/scripts/techsup/check_user_load.sh -u $cuser -p" 2>/dev/null)
 
 
