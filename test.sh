e4efd7f3a13705446dad79814f7e21636f764c83
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-02-08 19:51:29 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 clear
-# Визначення шляху до сайту
 
+# Визначення шляху до сайту
+SITE_PATH="$(pwd)"
+
+# Кольорові змінні
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+RESET='\033[0m'
+
+# Функція для виводу повідомлень
+log_message() {
+    echo -e "$1"
+}
+
+# Функція для отримання останнього запису в error_log
 get_last_error_log() {
-    error_log_file="./error_log"
+    error_log_file="$SITE_PATH/error_log"
+
     if [ -f "$error_log_file" ]; then 
         last_modified=$(stat -c "%y" "$error_log_file")  
         last_log=$(tail -n 1 "$error_log_file")  
@@ -14,4 +28,5 @@ get_last_error_log() {
     fi
 }
 
+# Виклик функції
 get_last_error_log
