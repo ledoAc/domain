77695cc6c410dc5698a968a067c8961e9ca9b076
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-02-25 15:03:32 +0200

    Create laravel.sh

---
+++ laravel.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+clear
+
+RED='\e[91m'
+GREEN='\e[92m'
+YELLOW='\e[93m'
+BLUE='\e[94m'
+NC='\e[0m' 
+
+if [ ! -f artisan ]; then
+    echo -e "${RED}Помилка: Схоже, що ви не знаходитеся в кореневій директорії Laravel.${NC}"
+    exit 1
+fi
+
+while true; do
+    
+    echo -e "${BLUE}Оберіть команду для виконання:${NC}"
+    echo -e "${YELLOW}1)${NC} Включити режим розробки"
+    echo -e "${YELLOW}2)${NC} Вийти з режиму розробки"
+    echo -e "${YELLOW}3)${NC} Інформація про додаток"
+    echo -e "${YELLOW}4)${NC} Очистити кеш"
+    echo -e "${YELLOW}5)${NC} Моніторинг бази даних"
+    echo -e "${YELLOW}6)${NC} Показати інформацію про БД"
+    echo -e "${YELLOW}7)${NC} Генерувати ключ додатку"
+    echo -e "${YELLOW}8)${NC} Включити DEBUG mode"
+    echo -e "${YELLOW}9)${NC} Вимкнути DEBUG mode"
+    echo -e "${YELLOW}10)${NC} Вийти"
+
+    read -p "Введіть номер команди: " choice
+
+    case $choice in
+        1)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan down
+            ;;
+        2)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan up
+            ;;
+        3)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan about
+            ;;
+        4)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan cache:clear
+            ;;
+        5)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan db:monitor
+            ;;
+        6)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan db:show
+            ;;
+        7)
+            echo -e "${GREEN}Перевірка......${NC}"
+            php artisan key:generate
+            ;;
+        8)
+            echo -e "${GREEN}Включаємо debug mode${NC}"
+            sed -i 's/APP_DEBUG=.*/APP_DEBUG=true/' .env && php artisan config:clear
+            echo -e "Дебаг мод включено успішно"
+            ;;
+        9)
+            echo -e "${GREEN}Вимикаємо debug mode${NC}"
+            sed -i 's/APP_DEBUG=.*/APP_DEBUG=false/' .env && php artisan config:clear
+            echo -e "Дебаг мод виключено успішно"
+            ;;
+        10)
+            echo -e "${RED}Вихід...${NC}"
+            exit 0
+            ;;
+        *)
+            echo -e "${RED}Невірний вибір, спробуйте ще раз.${NC}"
+            ;;
+     esac
+
+    echo ""
+    read -p "Натисніть Enter для продовження..." 
+done
