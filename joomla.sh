8943560ce15746c55a7b744a9c363042605ee0fb
ledoAc <164891135+ledoAc@users.noreply.github.com>
2025-02-25 13:10:30 +0200

    Update joomla.sh

--- joomla.sh
+++ joomla.sh
@@ -23,11 +23,12 @@ while true; do
     echo -e "${YELLOW}6)${NC} Переключити на сайті режим обслуговування"
     echo -e "${YELLOW}7)${NC} Виключити на сайті режим обслуговування"
     echo -e "${YELLOW}8)${NC} Список користувачів"
-    echo -e "${YELLOW}9)${NC} Скидання пароля користувача"
-    echo -e "${YELLOW}10)${NC} Переглянути error_log"
-    echo -e "${YELLOW}11)${NC} Перевірити дозволи на файли та папки"
-    echo -e "${YELLOW}12)${NC} Виправити дозволи файлів та папок"
-    echo -e "${YELLOW}13)${NC} Очищення кешу Joomla"
+    echo -e "${YELLOW}9)${NC} Додати користувача"
+    echo -e "${YELLOW}10)${NC} Скидання пароля користувача"
+    echo -e "${YELLOW}11)${NC} Переглянути error_log"
+    echo -e "${YELLOW}12)${NC} Перевірити дозволи на файли та папки"
+    echo -e "${YELLOW}13)${NC} Виправити дозволи файлів та папок"
+    echo -e "${YELLOW}14)${NC} Очищення кешу Joomla"
     echo -e "${RED}0)${NC} Вийти"
     echo ""
     read -p "Введіть номер опції: " choice
@@ -41,8 +42,9 @@ while true; do
         6) php "$CLI_PATH" site:down ;;
         7) php "$CLI_PATH" site:up ;;
         8) php "$CLI_PATH" user:list ;;
-        9) php "$CLI_PATH" user:reset-password ;;
-        10)
+        9) php "$CLI_PATH" user:add ;;
+        10) php "$CLI_PATH" user:reset-password ;;
+        11)
             if [ -f "$ERROR_LOG" ]; then
                 echo -e "${BLUE}Помилки з error_log:${NC}"
                 tail -n 5 "$ERROR_LOG"
@@ -50,18 +52,18 @@ while true; do
                 echo -e "${RED}Файл error_log не знайдено!${NC}"
             fi
             ;;
-        11)
+        12)
             echo -e "${BLUE}Перевірка дозволів файлів та папок у $ROOT_DIR...${NC}"
             find "$ROOT_DIR" -type d ! -perm 755 -exec echo -e "${RED}Папка з некоректними дозволами: {}${NC}" \;
             find "$ROOT_DIR" -type f ! -perm 644 -exec echo -e "${RED}Файл з некоректними дозволами: {}${NC}" \;
             ;;
-        12)
+        13)
             echo -e "${BLUE}Виправлення дозволів файлів та папок у $ROOT_DIR...${NC}"
             find "$ROOT_DIR" -type d ! -perm 755 -exec chmod 755 {} \;
             find "$ROOT_DIR" -type f ! -perm 644 -exec chmod 644 {} \;
             echo -e "${GREEN}Виправлення завершено!${NC}"
             ;;
-        13) php "$CLI_PATH" cache:clean | tee -a "$LOG_FILE" ;;
+        14) php "$CLI_PATH" cache:clean | tee -a "$LOG_FILE" ;;
                0) echo -e "${RED}Вихід...${NC}"; exit 0 ;;
         *) echo -e "${RED}Невірний вибір, спробуйте ще раз.${NC}"; sleep 2 ;;
     esac
